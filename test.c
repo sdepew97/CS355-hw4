c36265fb6d46ab0045b3c7c02724055855133bdb
Sarah Depew <sdepew@brynmawr.edu>
2018-03-14 20:39:42 -0600

    trying to join two

--- test.c
+++ test.c
@@ -11,6 +11,7 @@ int main() {
     thread_create(printHello, NULL, -1);
     thread_create(printHello, NULL, -1);
     thread_join(2);
+    printf("joining 1\n");
     thread_join(1);
 
     printf("Back to main\n");
