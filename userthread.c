7f8d454a2c5daf631f7f63958b39103dc82b9c30
Sarah Depew <sdepew@brynmawr.edu>
2018-03-15 20:59:38 -0600

    fixing code

--- userthread.c
+++ userthread.c
@@ -122,6 +122,7 @@ int thread_libterminate(void) {
 
 int thread_create(void (*func)(void *), void *arg, int priority) {
     printf("creating new thread %d\n", TID);
+    printList();
     //check type of scheduling
 
     //create a new context and TCB for the thread
