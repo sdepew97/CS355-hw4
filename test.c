aca3a1031b3956bda8be0bd2449a68aeab568951
Sarah Depew <sdepew@brynmawr.edu>
2018-03-16 00:20:53 -0600

    context switching

--- test.c
+++ test.c
@@ -9,7 +9,7 @@ void printHello () {
 int main() {
     thread_libinit(FIFO);
     thread_join(thread_create(printHello, NULL, -1));
-    thread_create(printHello, NULL, -1);
+    thread_join(thread_create(printHello, NULL, -1));
     thread_join(thread_create(printHello, NULL, -1));
 //    thread_join(thread_create(printHello, NULL, -1));
 //    thread_join(3);
