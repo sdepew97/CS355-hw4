287b5546cd87f2bd8d8fb470953d9f1dac792dae
Sarah Depew <sdepew@brynmawr.edu>
2018-03-18 10:47:53 -0600

    Dylan Tests

--- test.c
+++ test.c
@@ -264,8 +264,42 @@ void foo(void *arg) {
 
 }
 
+//
+/// /**
+// * Nested FIFO test
+// */
+//int main(void) {
+//    if (thread_libinit(FIFO) == -1)
+//        exit(EXIT_FAILURE);
+//
+//    char *arg = "Yay!";
+//
+//    int tid1 = thread_create(foo, arg, 1);
+//
+//    printf("More complicated FIFO test, with threads creating other threads.\n");
+//    printf("Assuming that the first thread is tid 1, it should finish in order\n");
+//    printf("2 -> 3 -> 4 -> 5 -> 6 -> 1\n");
+//
+//    if (thread_join(tid1) < 0)
+//        exit(EXIT_FAILURE);
+//
+//    if (thread_libterminate() == -1)
+//        exit(EXIT_FAILURE);
+//
+//
+//    exit(EXIT_SUCCESS);
+//}
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "userthread.h"
+
+void foo(void *arg) {
+    printf("%s\n", arg);
+}
+
 /**
- * Nested FIFO test
+ * Simple FIFO test
  */
 int main(void) {
     if (thread_libinit(FIFO) == -1)
@@ -275,16 +309,14 @@ int main(void) {
 
     int tid1 = thread_create(foo, arg, 1);
 
-    printf("More complicated FIFO test, with threads creating other threads.\n");
-    printf("Assuming that the first thread is tid 1, it should finish in order\n");
-    printf("2 -> 3 -> 4 -> 5 -> 6 -> 1\n");
+    printf("Basic FIFO test, testing only add and join.\n");
+    printf("Should print 'Yay!' if successful\n");
 
     if (thread_join(tid1) < 0)
         exit(EXIT_FAILURE);
 
     if (thread_libterminate() == -1)
         exit(EXIT_FAILURE);
-
 
     exit(EXIT_SUCCESS);
 }
