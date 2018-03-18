edc48c025a7522243e07e57dead3f53c3f0bc493
Sarah Depew <sdepew@brynmawr.edu>
2018-03-18 12:15:14 -0600

    Tests

--- test.c
+++ test.c
@@ -294,28 +294,38 @@
 #include <stdlib.h>
 #include "userthread.h"
 
-void foo(void *arg) {
-    printf("%s\n", arg);
+#define SUCCESS 0
+#define FAILURE -1
+#define PRIORITY 1
+
+void printHello() {
+    printf("Hello World\n");
 }
 
-/**
- * Simple FIFO test
+/*
+ * Simple FIFO test with main's functionality as a thread is tested.
  */
 int main(void) {
-    if (thread_libinit(FIFO) == -1)
+    if (thread_libinit(FIFO) == FAILURE)
         exit(EXIT_FAILURE);
 
-    char *arg = "Yay!";
+    printf("This is a FIFO test where main is tested as a thread.\n");
+    printf("Should run main->%d->%d and print Hello World, Back in Main, and Hello World if successful.\n");
 
-    int tid1 = thread_create(foo, arg, 1);
-
-    printf("Basic FIFO test, testing only add and join.\n");
-    printf("Should print 'Yay!' if successful\n");
-
-    if (thread_join(tid1) < 0)
+    int tid1 = thread_create(printHello, NULL, PRIORITY);
+    if (thread_join(tid1) == FAILURE)
         exit(EXIT_FAILURE);
 
-    if (thread_libterminate() == -1)
+    printf("Back in Main\n");
+
+    if (thread_join(tid1) == FAILURE)
+        exit(EXIT_FAILURE);
+
+    int tid2 = thread_create(printHello, NULL, PRIORITY);
+    if (thread_join(tid2) == FAILURE)
+        exit(EXIT_FAILURE);
+
+    if (thread_libterminate() == FAILURE)
         exit(EXIT_FAILURE);
 
     exit(EXIT_SUCCESS);
