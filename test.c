b98b26f22abd954ace5ebe7384cf37537dcb5177
Sarah Depew <sdepew@brynmawr.edu>
2018-03-19 17:12:06 -0400

    Testing

--- test.c
+++ test.c
@@ -119,60 +119,114 @@
 //    exit(EXIT_SUCCESS);
 //}
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <poll.h>
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <poll.h>
+//#include "userthread.h"
+//
+//void foo1() {
+//    for (int i = 0; i < 4; i++) {
+//        poll(NULL, 0, 1);
+//        thread_yield();
+//    }
+//}
+//
+//void foo200() {
+//    for (int i = 0; i < 4; i++) {
+//        poll(NULL, 0, 200);
+//        thread_yield();
+//    }
+//}
+//
+///**
+// * A simple test for SJF
+// */
+//int main(void) {
+//    if (thread_libinit(SJF) == -1)
+//        exit(EXIT_FAILURE);
+//
+//    int tid1 = thread_create(foo1, NULL, 1);
+//    int tid2 = thread_create(foo200, NULL, 1);
+//    int tid3 = thread_create(foo1, NULL, 1);
+//    int tid4 = thread_create(foo200, NULL, 1);
+//    int tid5 = thread_create(foo1, NULL, 1);
+//    int tid6 = thread_create(foo200, NULL, 1);
+//
+//    printf(" * A simple test for SJF scheduling\n");
+//    printf(" Assumptions\n");
+//    printf("(1) the default runtime value is less than 200 msec (real time rather than cputime).\n");
+//    printf(" * The threads with tid %d, %d and %d should end earlier than the threads with tid %d, %d, %d\n",
+//           tid1, tid3, tid5, tid2, tid4, tid6);
+//
+//    int n = 6;
+//    int tids[] = {tid1, tid2, tid3, tid4, tid5, tid6};
+//
+//    for (int i = 0; i < n; i++) {
+//        if (tids[i] == -1)
+//            exit(EXIT_FAILURE);
+//    }
+//
+//    for (int i = 0; i < n; i++) {
+//        if (thread_join(tids[i]) == -1)
+//            exit(EXIT_FAILURE);
+//    }
+//
+//    if (thread_libterminate() == -1)
+//        exit(EXIT_FAILURE);
+//
+//    exit(EXIT_SUCCESS);
+//}
+
+
 #include "userthread.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <poll.h>
+#include <stdlib.h>
 
-void foo1() {
-    for (int i = 0; i < 4; i++) {
-        poll(NULL, 0, 1);
-        thread_yield();
-    }
+
+void func(){
+    for(int i=0;i<10;i++)
+        poll(NULL, 0, 20);
 }
 
-void foo200() {
-    for (int i = 0; i < 4; i++) {
-        poll(NULL, 0, 200);
-        thread_yield();
-    }
+
+int testLl(){
+    printf("\n\nTesting thread_create() after the scheduler has been activated by thread_join()\n" );
+    printf("thread 1, 2, 3 should finish before 4, 5, 6 \n\n\n" );
+
+
+    thread_libinit(FIFO); // FIFO SJF PRIORITY
+
+    //create
+    int tid1 = thread_create(func, NULL, 0);
+    int tid2 = thread_create(func, NULL, -1);
+    int tid3 = thread_create(func, NULL, 1);
+
+
+    // join
+    thread_join(tid1);
+    thread_join(tid2);
+    thread_join(tid3);
+
+    int tid4 = thread_create(func, NULL, 1);
+    int tid5 = thread_create(func, NULL, 1);
+    int tid6 = thread_create(func, NULL, 1);
+
+    thread_join(tid4);
+    thread_join(tid5);
+    thread_join(tid6);
+
+
+
+    // term
+    thread_libterminate();
+
+    return 1;
 }
 
-/**
- * A simple test for SJF
- */
-int main(void) {
-    if (thread_libinit(SJF) == -1)
-        exit(EXIT_FAILURE);
 
-    int tid1 = thread_create(foo1, NULL, 1);
-    int tid2 = thread_create(foo200, NULL, 1);
-    int tid3 = thread_create(foo1, NULL, 1);
-    int tid4 = thread_create(foo200, NULL, 1);
-    int tid5 = thread_create(foo1, NULL, 1);
-    int tid6 = thread_create(foo200, NULL, 1);
-
-    printf(" * A simple test for SJF scheduling\n");
-    printf(" Assumptions\n");
-    printf("(1) the default runtime value is less than 200 msec (real time rather than cputime).\n");
-    printf(" * The threads with tid %d, %d and %d should end earlier than the threads with tid %d, %d, %d\n",
-           tid1, tid3, tid5, tid2, tid4, tid6);
-
-    int n = 6;
-    int tids[] = {tid1, tid2, tid3, tid4, tid5, tid6};
-
-    for (int i = 0; i < n; i++) {
-        if (tids[i] == -1)
-            exit(EXIT_FAILURE);
-    }
-
-    for (int i = 0; i < n; i++) {
-        if (thread_join(tids[i]) == -1)
-            exit(EXIT_FAILURE);
-    }
-
-    if (thread_libterminate() == -1)
-        exit(EXIT_FAILURE);
-
+int main(){
+    testLl();
     exit(EXIT_SUCCESS);
 }
