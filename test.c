8f321a9fa6f18d7598803c79664639d156972c76
Sarah Depew <sdepew@brynmawr.edu>
2018-03-19 16:31:45 -0400

    Testing SJF

--- test.c
+++ test.c
@@ -1,6 +1,54 @@
+//#include <stdio.h>
+//#include "userthread.h"
+//#include "logger.h"
+//
+//void printHello () {
+//    printf("Hello world\n");
+//}
+//
+//void tryYield() {
+//    printf("start yield\n");
+//    thread_yield();
+//    printf("end yield\n");
+//    printHello();
+//}
+//
+//int main() {
+////    thread_libinit(FIFO);
+//    thread_libinit(SJF);
+//    thread_create(printHello, NULL, -1);
+//    thread_create(tryYield, NULL, -1);
+////    thread_create(printHello, NULL, -1);
+//
+//    //expected is that 2 runs last...
+//
+////    thread_join(thread_create(printHello, NULL, -1));
+////    thread_join(thread_create(printHello, NULL, -1));
+////    thread_join(thread_create(printHello, NULL, -1));
+////    thread_join(thread_create(printHello, NULL, -1));
+//    printf("joining 2\n");
+//    thread_join(2);
+//    printf("joining 1\n");
+//    thread_join(1);
+//
+////    thread_join(3);
+////    printf("joining 2\n");
+////    printf("%d\n", thread_join(2));
+//
+//
+//    printf("Back to main\n");
+//
+//    return 0;
+//}
+
 #include <stdio.h>
+#include <stdlib.h>
 #include "userthread.h"
 #include "logger.h"
+
+#define SUCCESS 0
+#define FAILURE -1
+#define PRIORITY 1
 
 void printHello () {
     printf("Hello world\n");
@@ -14,29 +62,33 @@ void tryYield() {
 }
 
 int main() {
-//    thread_libinit(FIFO);
-    thread_libinit(SJF);
-    thread_create(printHello, NULL, -1);
-    thread_create(tryYield, NULL, -1);
-//    thread_create(printHello, NULL, -1);
+    if(thread_libinit(SJF) == FAILURE)
+        exit(EXIT_FAILURE);
 
-    //expected is that 2 runs last...
+    int tid1 = thread_create(printHello, NULL, -1);
 
-//    thread_join(thread_create(printHello, NULL, -1));
-//    thread_join(thread_create(printHello, NULL, -1));
-//    thread_join(thread_create(printHello, NULL, -1));
-//    thread_join(thread_create(printHello, NULL, -1));
+    if(tid1 == FAILURE)
+        exit(EXIT_FAILURE);
+
+    int tid2 = thread_create(tryYield, NULL, -1);
+
+    if(tid2 == FAILURE)
+        exit(EXIT_FAILURE);
+
     printf("joining 2\n");
-    thread_join(2);
+    if(thread_join(tid2) == FAILURE)
+        exit(EXIT_FAILURE);
+
     printf("joining 1\n");
-    thread_join(1);
 
-//    thread_join(3);
-//    printf("joining 2\n");
-//    printf("%d\n", thread_join(2));
-
+    if(thread_join(tid1) == FAILURE)
+        exit(EXIT_FAILURE);
 
     printf("Back to main\n");
 
-    return 0;
+    if (thread_libterminate() == FAILURE)
+        exit(EXIT_FAILURE);
+
+    printf("Congratulations, your test was successful!\n");
+    exit(EXIT_SUCCESS);
 }
