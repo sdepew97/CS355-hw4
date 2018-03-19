8144c7523fbfb1db42f8b50f6f9d9eed28aea2bf
Sarah Depew <sdepew@brynmawr.edu>
2018-03-18 20:52:25 -0400

    testing

--- test.c
+++ test.c
@@ -41,52 +41,100 @@
 //    return 0;
 //}
 
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <poll.h>
+//#include "userthread.h"
+//
+//int idx = 0;
+//int created_tids[3] = { -1, -1, -1 };
+//
+//void foo() {}
+//void foo_create() {
+//    created_tids[idx] = thread_create(foo, NULL, 1);
+//    thread_join(created_tids[idx++]);
+//}
+//
+//int main(void) {
+//    if (thread_libinit(FIFO) == -1)
+//        exit(EXIT_FAILURE);
+//
+//    int tid1 = thread_create(foo_create, NULL, 1);
+//    int tid2 = thread_create(foo_create, NULL, 1);
+//    int tid3 = thread_create(foo_create, NULL, 1);
+//
+//    int n = 3;
+//    int tids[] = { tid1, tid2, tid3 };
+//
+//    for (int i = 0; i < n; i++)  {
+//        if (tids[i] == -1)
+//            exit(EXIT_FAILURE);
+//    }
+//
+//    for (int i = 0; i < n; i++)  {
+//        if (thread_join(tids[i]) == -1)
+//            exit(EXIT_FAILURE);
+//    }
+//
+//    for (int i = 0; i < 3; i++)  {
+//        if (created_tids[i] == -1)
+//            exit(EXIT_FAILURE);
+//    }
+//
+//    printf(" * Testing thread_create within thread routines\n");
+//    printf(" * Threads should end in this order ");
+//    printf(" %d -> %d -> %d", created_tids[0], created_tids[1], created_tids[2]);
+//    printf(" -> %d -> %d -> %d \n", tid1, tid2, tid3);
+//
+//    if (thread_libterminate() == -1)
+//        exit(EXIT_FAILURE);
+//
+//    exit(EXIT_SUCCESS);
+//}
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <poll.h>
 #include "userthread.h"
 
-int idx = 0;
-int created_tids[3] = { -1, -1, -1 };
+#define FAILURE -1
 
 void foo() {}
-void foo_create() {
-    created_tids[idx] = thread_create(foo, NULL, 1);
-    thread_join(created_tids[idx++]);
+
+void foo_yield() {
+    thread_yield();
+}
+
+void foo_join(void *tid) {
+    thread_join(*((int *)tid));
 }
 
 int main(void) {
-    if (thread_libinit(FIFO) == -1)
+    if (thread_libinit(FIFO) == FAILURE)
         exit(EXIT_FAILURE);
 
-    int tid1 = thread_create(foo_create, NULL, 1);
-    int tid2 = thread_create(foo_create, NULL, 1);
-    int tid3 = thread_create(foo_create, NULL, 1);
+    int tid1 = thread_create(foo_yield, NULL, 0);
+    int tid2 = thread_create(foo_join, &tid1, 0);
+    int tid3 = thread_create(foo_join, &tid2, 0);
+    int tid4 = thread_create(foo_join, &tid3, 0);
+    int tid5 = thread_create(foo_join, &tid4, 0);
+    int tid6 = thread_create(foo_join, &tid5, 0);
 
-    int n = 3;
-    int tids[] = { tid1, tid2, tid3 };
+    int n = 6;
+    int tids[] = {tid1, tid2, tid3, tid4, tid5, tid6};
 
-    for (int i = 0; i < n; i++)  {
+    printf(" * Testing a chain of joins for FIFO\n");
+    printf(" * Threads should in this order: %d -> %d -> %d -> %d -> %d -> %d\n",
+           tid1, tid2, tid3, tid4, tid5, tid6);
+
+    for (int i = 0; i < n; i++) {
         if (tids[i] == -1)
             exit(EXIT_FAILURE);
     }
 
-    for (int i = 0; i < n; i++)  {
-        if (thread_join(tids[i]) == -1)
-            exit(EXIT_FAILURE);
-    }
+    if (thread_join(tid6) == -1)
+        exit(EXIT_FAILURE);
 
-    for (int i = 0; i < 3; i++)  {
-        if (created_tids[i] == -1)
-            exit(EXIT_FAILURE);
-    }
-
-    printf(" * Testing thread_create within thread routines\n");
-    printf(" * Threads should end in this order ");
-    printf(" %d -> %d -> %d", created_tids[0], created_tids[1], created_tids[2]);
-    printf(" -> %d -> %d -> %d \n", tid1, tid2, tid3);
-
-    if (thread_libterminate() == -1)
+    if (thread_libterminate() == FAILURE)
         exit(EXIT_FAILURE);
 
     exit(EXIT_SUCCESS);
