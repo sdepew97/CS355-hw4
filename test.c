8f2498113e67f8c1c7bcb24751a1c9a726500440
Sarah Depew <sdepew@brynmawr.edu>
2018-03-20 13:11:21 -0400

    Testing

--- test.c
+++ test.c
@@ -356,41 +356,93 @@
 //    exit(EXIT_SUCCESS);
 //}
 
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include "userthread.h"
+//
+//#define FAILURE -1
+//
+//void foo() {
+//    printf("Woo!\n");
+//}
+//
+//int main(void) {
+//    printf("Testing some misuses of threads\n");
+//    printf("This assumes that the priority queue range is from -1 to 1\n");
+//    printf("because it tests adding a thread with priority 100\n");
+//    printf("On success, it prints 'Woo!', does not crash, and does not cause any memory leaks\n");
+//
+//    int tidx = thread_create(foo, NULL, 0);
+//    if (tidx != -1)
+//        exit(EXIT_FAILURE);
+//
+////    thread_libterminate();
+//
+//    if (thread_libinit(PRIORITY) == FAILURE)
+//        exit(EXIT_FAILURE);
+//
+//    int tid1 = thread_create(foo, NULL, 100);
+//    if (tid1 == -1)
+//        printf("failure to create\n");
+//        exit(EXIT_FAILURE);
+//
+//    if (thread_join(tid1) == -1)
+//        exit(EXIT_FAILURE);
+//
+////    if (thread_libterminate() == FAILURE)
+////        exit(EXIT_FAILURE);
+//
+//    exit(EXIT_SUCCESS);
+//}
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "userthread.h"
 
-#define FAILURE -1
+#define FAIL -1
 
-void foo() {
-    printf("Woo!\n");
+void highest(void *arg)
+{
+    printf("This thread's priority is highest, -1.\n");
+}
+
+void second(void *arg)
+{
+    printf("This thread's priority is second priority, 0.\n");
+}
+
+void lowest(void *arg)
+{
+    printf("This thread's priority is lowest, 1.\n");
 }
 
 int main(void) {
-    printf("Testing some misuses of threads\n");
-    printf("This assumes that the priority queue range is from -1 to 1\n");
-    printf("because it tests adding a thread with priority 100\n");
-    printf("On success, it prints 'Woo!', does not crash, and does not cause any memory leaks\n");
-
-    int tidx = thread_create(foo, NULL, 0);
-    if (tidx != -1)
+    if (thread_libinit(PRIORITY) == FAIL)
         exit(EXIT_FAILURE);
+    int tid1 = thread_create(highest, NULL, -1);
+    int tid2 = thread_create(second, NULL, 0);
+    int tid3 = thread_create(lowest, NULL, 1);
+    int tid4 = thread_create(second, NULL, 0);
+    int tid5 = thread_create(highest, NULL, -1);
+    int tid6 = thread_create(highest, NULL, -1);
+    int tid7 = thread_create(second, NULL, 0);
+    int tid8 = thread_create(highest, NULL, -1);
+    int tid9 = thread_create(lowest, NULL, 1);
 
-//    thread_libterminate();
-
-    if (thread_libinit(PRIORITY) == FAILURE)
+    printf("* A simple test case for PRIORITY scheduling policy.\n");
+    printf("* Testing only thread_create and thread_join.\n");
+    int num = 9;
+    int tids[] = {tid1, tid2, tid3, tid4, tid5, tid6, tid7, tid8, tid9};
+    for (int i = 0; i < num; i++) {
+        if (tids[i] == FAIL)
+            exit(EXIT_FAILURE);
+    }
+    for (int i = 0; i < num; i++) {
+        if (thread_join(tids[i]) == FAIL)
+            exit(EXIT_FAILURE);
+    }
+    if (thread_libterminate() == FAIL)
         exit(EXIT_FAILURE);
-
-    int tid1 = thread_create(foo, NULL, 100);
-    if (tid1 == -1)
-        printf("failure to create\n");
-        exit(EXIT_FAILURE);
-
-    if (thread_join(tid1) == -1)
-        exit(EXIT_FAILURE);
-
-//    if (thread_libterminate() == FAILURE)
-//        exit(EXIT_FAILURE);
 
     exit(EXIT_SUCCESS);
 }
