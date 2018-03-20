67560b26ebd46996acd46ba8f77e92fb278b9c6e
Sarah Depew <sdepew@brynmawr.edu>
2018-03-20 13:13:18 -0400

    Testing

--- test.c
+++ test.c
@@ -395,44 +395,87 @@
 //    exit(EXIT_SUCCESS);
 //}
 
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include "userthread.h"
+//
+//#define FAIL -1
+//
+//void highest(void *arg)
+//{
+//    printf("This thread's priority is highest, -1.\n");
+//}
+//
+//void second(void *arg)
+//{
+//    printf("This thread's priority is second priority, 0.\n");
+//}
+//
+//void lowest(void *arg)
+//{
+//    printf("This thread's priority is lowest, 1.\n");
+//}
+//
+//int main(void) {
+//    if (thread_libinit(PRIORITY) == FAIL)
+//        exit(EXIT_FAILURE);
+//    int tid1 = thread_create(highest, NULL, -1);
+//    int tid2 = thread_create(second, NULL, 0);
+//    int tid3 = thread_create(lowest, NULL, 1);
+//    int tid4 = thread_create(second, NULL, 0);
+//    int tid5 = thread_create(highest, NULL, -1);
+//    int tid6 = thread_create(highest, NULL, -1);
+//    int tid7 = thread_create(second, NULL, 0);
+//    int tid8 = thread_create(highest, NULL, -1);
+//    int tid9 = thread_create(lowest, NULL, 1);
+//
+//    printf("* A simple test case for PRIORITY scheduling policy.\n");
+//    printf("* Testing only thread_create and thread_join.\n");
+//    int num = 9;
+//    int tids[] = {tid1, tid2, tid3, tid4, tid5, tid6, tid7, tid8, tid9};
+//    for (int i = 0; i < num; i++) {
+//        if (tids[i] == FAIL)
+//            exit(EXIT_FAILURE);
+//    }
+//    for (int i = 0; i < num; i++) {
+//        if (thread_join(tids[i]) == FAIL)
+//            exit(EXIT_FAILURE);
+//    }
+//
+//    printf("Success!\n");
+////    if (thread_libterminate() == FAIL)
+////        exit(EXIT_FAILURE);
+//
+//    exit(EXIT_SUCCESS);
+//}
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <poll.h>
 #include "userthread.h"
 
 #define FAIL -1
 
-void highest(void *arg)
+void big(void *arg)
 {
-    printf("This thread's priority is highest, -1.\n");
-}
-
-void second(void *arg)
-{
-    printf("This thread's priority is second priority, 0.\n");
-}
-
-void lowest(void *arg)
-{
-    printf("This thread's priority is lowest, 1.\n");
+    poll(NULL, 0, 200);
 }
 
 int main(void) {
     if (thread_libinit(PRIORITY) == FAIL)
         exit(EXIT_FAILURE);
-    int tid1 = thread_create(highest, NULL, -1);
-    int tid2 = thread_create(second, NULL, 0);
-    int tid3 = thread_create(lowest, NULL, 1);
-    int tid4 = thread_create(second, NULL, 0);
-    int tid5 = thread_create(highest, NULL, -1);
-    int tid6 = thread_create(highest, NULL, -1);
-    int tid7 = thread_create(second, NULL, 0);
-    int tid8 = thread_create(highest, NULL, -1);
-    int tid9 = thread_create(lowest, NULL, 1);
+    int tid1 = thread_create(big, NULL, -1);
+    int tid2 = thread_create(big, NULL, 0);
+    int tid3 = thread_create(big, NULL, 1);
+    int tid4 = thread_create(big, NULL, -1);
+    int tid5 = thread_create(big, NULL, 0);
+    int tid6 = thread_create(big, NULL, 1);
 
-    printf("* A simple test case for PRIORITY scheduling policy.\n");
-    printf("* Testing only thread_create and thread_join.\n");
-    int num = 9;
-    int tids[] = {tid1, tid2, tid3, tid4, tid5, tid6, tid7, tid8, tid9};
+    printf("* A intense test case for PRIORITY scheduling policy handling a big job.\n");
+    printf("* Testing thread_create, thread_join and sigalarm working or not.\n");
+    printf("* You should see threads ending from highest to lowest and yielding for running too long.\n");
+    int num = 6;
+    int tids[] = {tid1, tid2, tid3, tid4, tid5, tid6};
     for (int i = 0; i < num; i++) {
         if (tids[i] == FAIL)
             exit(EXIT_FAILURE);
@@ -441,10 +484,8 @@ int main(void) {
         if (thread_join(tids[i]) == FAIL)
             exit(EXIT_FAILURE);
     }
-
-    printf("Success!\n");
-//    if (thread_libterminate() == FAIL)
-//        exit(EXIT_FAILURE);
+    if (thread_libterminate() == FAIL)
+        exit(EXIT_FAILURE);
 
     exit(EXIT_SUCCESS);
 }
