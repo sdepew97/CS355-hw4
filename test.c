f278e0774344b8c855562d1517a1ce423de9ba8d
Sarah Depew <sdepew@brynmawr.edu>
2018-03-21 01:17:53 -0400

    Testing

--- test.c
+++ test.c
@@ -3,16 +3,32 @@
 #include "userthread.h"
 
 void foo(void *arg) {
+    printf("Hello world!\n");
 }
 
 /**
  * Simple FIFO test
  */
 int main(void) {
-    printf("Testing with wrong policy\n");
-    if (thread_libinit(100) == -1)
+    if (thread_libinit(FIFO) == -1)
         exit(EXIT_FAILURE);
 
+
+    int tid1 = thread_create(foo, NULL, 1);
+    int tid2 = thread_create(foo, NULL, 100);
+    int tid3 = thread_create(foo, NULL, -100);
+
+    printf("Tesing invalid policy number\n");
+    printf("Should print exactly one 'Hello world' on success\n");
+
+    if (thread_join(tid1) < 0)
+        exit(EXIT_FAILURE);
+
+    if (thread_join(tid2) < 0)
+        exit(EXIT_FAILURE);
+
+    if (thread_join(tid3) < 0)
+        exit(EXIT_FAILURE);
 
     if (thread_libterminate() == -1)
         exit(EXIT_FAILURE);
