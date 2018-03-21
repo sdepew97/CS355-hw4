2fbea403a49a51475ee042d9297315df695c0b07
Sarah Depew <sdepew@brynmawr.edu>
2018-03-21 01:36:45 -0400

    Testing

--- test.c
+++ test.c
@@ -1,60 +1,41 @@
-/*
-Test of thread_yield
-Uses two threads with functions f1, f2
-thread with f1 runs first, then calls thread_yield
-so thread 2 with f2 runs for its entirety
-then thread 1 with f1 resumes and finishes
-Expected output:
-from f1
-from f2
-from f2
-from f2
-from f2
-from f2
-from f1
-from f1
-from f1
-from f1
-terminated
-*/
-
-#include <ucontext.h>
-#include <sys/types.h>
-#include <signal.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
-
+#include <stdint.h>
+#include <string.h>
+#include <poll.h>
 #include "userthread.h"
-// #include "scheduler.h"
 
-void f1() {
-    for (int i = 0; i < 5; i++) {
-        printf("\nfrom f1\n\n");
-        sleep(1);
-        thread_yield();
-    }
-}
-void f2() {
-    for (int i = 0; i < 5; i++) {
-        printf("\nfrom f2\n\n");
-        sleep(1);
-    }
+#define N 128
+
+void foo(void *) {
+    poll(NULL, 0, 1);
 }
 
-int main() {
-    int tid, tid2;
+int main(void) {
+    printf(" * Running 129 threads! \n");
 
-    thread_libinit(FIFO);
+    if (thread_libinit(FIFO) == -1)
+        exit(EXIT_FAILURE);
 
-    // create threads with functions f1, f2
-    tid = thread_create(f1, NULL, 0);
-    tid2 = thread_create(f2, NULL, 0);
+    int tids[N];
+    memset(tids, -1, sizeof(tids));
 
-    thread_join(tid);
-    thread_join(tid2);
+    for (int i = 0; i < N; i++)  {
+        tids[i] = thread_create(foo, NULL, 1);
+    }
 
-    thread_libterminate();
-    printf("terminated\n");
-    return 0;
+    for (int i = 0; i < N; i++)  {
+        if (tids[i] == -1)
+            exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < N; i++)  {
+        if (thread_join(tids[i]) == -1)
+            exit(EXIT_FAILURE);
+    }
+
+    if (thread_libterminate() == -1)
+        exit(EXIT_FAILURE);
+
+    exit(EXIT_SUCCESS);
 }
