cf6eedd4c2a4eefe4a6e7464d956659b5a146222
Sarah Depew <sdepew@brynmawr.edu>
2018-03-21 02:33:26 -0400

    Testing

--- test.c
+++ test.c
@@ -2,15 +2,7 @@
 #include <stdlib.h>
 #include "userthread.h"
 
-int tid_1, tid_2;
-
 void hello(void *arg) {
-    thread_join(tid_2);
-    printf("%s\n", arg);
-}
-
-void hello2(void *arg) {
-    thread_join(tid_1);
     printf("%s\n", arg);
 }
 
@@ -18,14 +10,15 @@ int main(void) {
     if (thread_libinit(FIFO) == -1) exit(EXIT_FAILURE);
 
     char *hello_str = "Hello, world!";
-    tid_1 = thread_create(hello, hello_str, 0);
-    tid_2 = thread_create(hello2, hello_str, 0);
+    int tid_1 = thread_create(hello, hello_str, 0);
 
-    printf("Test case for FIFO. 2 threads join each other to create deadlock.\n");
-    printf("The program gets stuck and prints nothing on success.\n");
+    printf("Test case for FIFO. Attempt to join an non-existing thread.\n");
+    printf("Print \"Failed to join thread.\" on success.\n");
 
-    if (thread_join(tid_1) < 0) exit(EXIT_FAILURE);
-    if (thread_join(tid_2) < 0) exit(EXIT_FAILURE);
+    if (thread_join(-1) < 0) {
+        printf("Failed to join thread.\n");
+        exit(EXIT_FAILURE);
+    }
 
     if (thread_libterminate() == -1) exit(EXIT_FAILURE);
 
