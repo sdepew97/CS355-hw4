dc127812c590333c3e539d1dc569db4387f42642
Sarah Depew <sdepew@brynmawr.edu>
2018-03-21 01:31:21 -0400

    Testing

--- test.c
+++ test.c
@@ -2,50 +2,46 @@
 #include <stdlib.h>
 #include "userthread.h"
 
-void foo(void* args) {}
-void bar(void* args) {}
+void foo_yield(void* args) {
+    for (int i = 0; i < 100; i ++)
+        thread_yield();
+}
 
+/**
+ * A simple test for PRIORITY
+ * Expected Log: thread 1 to thread 3 end in order.
+ */
 int main(void) {
-    printf("Testing simple misuses\n");
-    printf("Shouldn't cause any crash on success\n");
-    thread_libterminate();
-    // the following three should return -1
-    if (thread_create(foo, NULL, 0) != -1)
-        exit(EXIT_FAILURE);
-    if (thread_create(bar, NULL, 0) != -1)
-        exit(EXIT_FAILURE);
-    if (thread_join(1) != -1)
+    if (thread_libinit(PRIORITY) == -1)
         exit(EXIT_FAILURE);
 
-    if (thread_libinit(FIFO) == -1)
-        exit(EXIT_FAILURE);
+    int tid1 = thread_create(foo_yield, NULL, -1);
+    int tid2 = thread_create(foo_yield, NULL, 0);
+    int tid3 = thread_create(foo_yield, NULL, 0);
+    int tid4 = thread_create(foo_yield, NULL, -1);
+    int tid5 = thread_create(foo_yield, NULL, 0);
+    int tid6 = thread_create(foo_yield, NULL, 0);
 
-    int tid1 = thread_create(foo, NULL, 0);
-    int tid2 = thread_create(foo, NULL, 0);
-    int tid3 = thread_create(foo, NULL, 0);
-    int tid4 = thread_create(foo, NULL, 0);
-    int tid5 = thread_create(foo, NULL, 0);
-    int tid6 = thread_create(foo, NULL, 0);
+    printf("Testing PRIORITY with multiple yield\n");
+    printf("On success, thread 1 and thread 4 should end ahead of all other threads\n");
 
     int n  = 6;
-    int tids[] = { tid1, tid2, tid3, tid4, tid5, tid6 };
+    int tids[] = { tid1, tid2, tid3 , tid4, tid5, tid6};
 
     for (int i = 0; i < n; i++)  {
         if (tids[i] == -1)
             exit(EXIT_FAILURE);
     }
 
-    if (thread_join(tid6) == -1)
-        exit(EXIT_FAILURE);
+    for (int i = 0; i < n; i++)  {
+        if (thread_join(tids[i]) == -1) {
+            exit(EXIT_FAILURE);
+        }
+    }
 
     if (thread_libterminate() == -1)
         exit(EXIT_FAILURE);
 
-
-    // more misuses...
-    thread_libterminate();
-    thread_join(123132);
-
-
+    printf(" Exit success\n");
     exit(EXIT_SUCCESS);
 }
