930ab253fe62738eb74bb7457f0ad70e04c69908
Sarah Depew <sdepew@brynmawr.edu>
2018-03-21 00:32:56 -0400

    Testing

--- test.c
+++ test.c
@@ -27,11 +27,10 @@ int main(void) {
         exit(EXIT_FAILURE);
     }
 
-    int tid1;
-//    int tid1 = thread_create(fifo_misuse_foo, NULL, 100);
-//    if (tid1 != FAILURE) {
-//        exit(EXIT_FAILURE);
-//    }
+    int tid1 = thread_create(fifo_misuse_foo, NULL, 100);
+    if (tid1 != FAILURE) {
+        exit(EXIT_FAILURE);
+    }
 
     tid1 = thread_create(fifo_misuse_foo, NULL, -1);
     if (tid1 == FAILURE) {
