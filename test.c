3364bd9ea8e0d016b20e029a9000695dff92ad2e
Sarah Depew <sdepew@brynmawr.edu>
2018-03-21 00:40:10 -0400

    Testing

--- test.c
+++ test.c
@@ -1,51 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include "userthread.h"
 
-#define FAILURE -1
-
-void fifo_misuse_foo() {
-    printf("Woo!\n");
+void inner_foo(void *arg) {
+    thread_yield();
 }
 
+
+void foo(void *arg) {
+    int tid;
+    for (int i = 0; i < 5; ++i)
+    {
+        int tid = thread_create(inner_foo, NULL, 1);
+
+        if (tid < 0)
+            exit(EXIT_FAILURE);
+
+        if (thread_join(tid) < 0)
+            exit(EXIT_FAILURE);
+    }
+
+}
+
+/**
+ * Nested FIFO test
+ */
 int main(void) {
-    printf("Testing some misuses of threads\n");
-    printf("This assumes that the priority queue range is from -1 to 1\n");
-    printf("because it tests adding a thread with priority 100\n");
-    printf("On success, it prints 'Woo!', does not crash, and does not cause any memory leaks\n");
-
-    int tidx = thread_create(FIFO, NULL, 0);
-    if (tidx != -1)
+    if (thread_libinit(FIFO) == -1)
         exit(EXIT_FAILURE);
 
-    // thread lib terminate could return success or failure,
-    // just make sure no memory leaks or crashes occur
-    thread_libterminate();
-    thread_libterminate();
+    char *arg = "Yay!";
 
-    if (thread_libinit(FIFO) == FAILURE) {
-        exit(EXIT_FAILURE);
-    }
-    int tid1;
+    int tid1 = thread_create(foo, arg, 1);
 
-    tid1 = thread_create(fifo_misuse_foo, NULL, 100);
-    if (tid1 != FAILURE) {
-        exit(EXIT_FAILURE);
-    }
+    printf("More complicated FIFO test, with threads creating other threads.\n");
+    printf("Assuming that the first thread is tid 1, it should finish in order\n");
+    printf("2 -> 3 -> 4 -> 5 -> 6 -> 1\n");
 
-    tid1 = thread_create(fifo_misuse_foo, NULL, -1);
-    if (tid1 == FAILURE) {
+    if (thread_join(tid1) < 0)
         exit(EXIT_FAILURE);
-    }
 
-    printf("got here\n");
-    if (thread_join(tid1) == FAILURE) {
+    if (thread_libterminate() == -1)
         exit(EXIT_FAILURE);
-    }
 
-    if (thread_libterminate() == FAILURE) {
-        exit(EXIT_FAILURE);
-    }
 
     exit(EXIT_SUCCESS);
 }
