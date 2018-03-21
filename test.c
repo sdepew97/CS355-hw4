ab61f29a7c567cc466923c40dc2f63d62af2e7b8
Sarah Depew <sdepew@brynmawr.edu>
2018-03-20 20:54:27 -0400

    Testing Priority

--- test.c
+++ test.c
@@ -449,89 +449,89 @@
 //    exit(EXIT_SUCCESS);
 //}
 
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <poll.h>
-//#include "userthread.h"
-//
-//#define FAIL -1
-//
-//void big(void *arg)
-//{
-//    poll(NULL, 0, 1000);
-//    printf("done with poll\n");
-//}
-//
-//int main(void) {
-//    if (thread_libinit(PRIORITY) == FAIL)
-//        exit(EXIT_FAILURE);
-//    int tid1 = thread_create(big, NULL, -1);
-//    int tid2 = thread_create(big, NULL, 0);
-//    int tid3 = thread_create(big, NULL, 1);
-//    int tid4 = thread_create(big, NULL, -1);
-//    int tid5 = thread_create(big, NULL, 0);
-//    int tid6 = thread_create(big, NULL, 1);
-//
-//    printf("* A intense test case for PRIORITY scheduling policy handling a big job.\n");
-//    printf("* Testing thread_create, thread_join and sigalarm working or not.\n");
-//    printf("* You should see threads ending from highest to lowest and yielding for running too long.\n");
-//    int num = 6;
-//    int tids[] = {tid1, tid2, tid3, tid4, tid5, tid6};
-//    for (int i = 0; i < num; i++) {
-//        if (tids[i] == FAIL)
-//            exit(EXIT_FAILURE);
-//    }
-//    for (int i = 0; i < num; i++) {
-//        if (thread_join(tids[i]) == FAIL)
-//            exit(EXIT_FAILURE);
-//    }
-//    if (thread_libterminate() == FAIL)
-//        exit(EXIT_FAILURE);
-//
-//    printf("Success!\n");
-//
-//    exit(EXIT_SUCCESS);
-//}
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <poll.h>
 #include "userthread.h"
 
-void foo_yield(void) {
-    for (int i = 0; i < 100; i ++)
-        thread_yield();
+#define FAIL -1
+
+void big(void *arg)
+{
+    poll(NULL, 0, 1000);
+    printf("done with poll\n");
 }
 
-/**
- * A simple test for PRIORITY
- * Expected Log: thread 1 to thread 3 end in order.
- */
 int main(void) {
-    if (thread_libinit(PRIORITY) == -1)
+    if (thread_libinit(PRIORITY) == FAIL)
         exit(EXIT_FAILURE);
+    int tid1 = thread_create(big, NULL, -1);
+    int tid2 = thread_create(big, NULL, 0);
+    int tid3 = thread_create(big, NULL, 1);
+    int tid4 = thread_create(big, NULL, -1);
+    int tid5 = thread_create(big, NULL, 0);
+    int tid6 = thread_create(big, NULL, 1);
 
-    int tid1 = thread_create(foo_yield, NULL, -1);
-    int tid2 = thread_create(foo_yield, NULL, 0);
-    int tid3 = thread_create(foo_yield, NULL, 1);
-
-    printf(" * A simple test for PRIORITY scheduling\n");
-    printf(" * Threads should in this order: %d -> %d -> %d\n", tid1, tid2, tid3);
-
-    int n = 3;
-    int tids[] = {tid1, tid2, tid3};
-
-    for (int i = 0; i < n; i++) {
-        if (tids[i] == -1)
+    printf("* A intense test case for PRIORITY scheduling policy handling a big job.\n");
+    printf("* Testing thread_create, thread_join and sigalarm working or not.\n");
+    printf("* You should see threads ending from highest to lowest and yielding for running too long.\n");
+    int num = 6;
+    int tids[] = {tid1, tid2, tid3, tid4, tid5, tid6};
+    for (int i = 0; i < num; i++) {
+        if (tids[i] == FAIL)
             exit(EXIT_FAILURE);
     }
-
-    for (int i = 0; i < n; i++) {
-        if (thread_join(tids[i]) == -1)
+    for (int i = 0; i < num; i++) {
+        if (thread_join(tids[i]) == FAIL)
             exit(EXIT_FAILURE);
     }
-
-    if (thread_libterminate() == -1)
+    if (thread_libterminate() == FAIL)
         exit(EXIT_FAILURE);
+
+    printf("Success!\n");
 
     exit(EXIT_SUCCESS);
 }
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include "userthread.h"
+//
+//void foo_yield(void) {
+//    for (int i = 0; i < 100; i ++)
+//        thread_yield();
+//}
+//
+///**
+// * A simple test for PRIORITY
+// * Expected Log: thread 1 to thread 3 end in order.
+// */
+//int main(void) {
+//    if (thread_libinit(PRIORITY) == -1)
+//        exit(EXIT_FAILURE);
+//
+//    int tid1 = thread_create(foo_yield, NULL, -1);
+//    int tid2 = thread_create(foo_yield, NULL, 0);
+//    int tid3 = thread_create(foo_yield, NULL, 1);
+//
+//    printf(" * A simple test for PRIORITY scheduling\n");
+//    printf(" * Threads should in this order: %d -> %d -> %d\n", tid1, tid2, tid3);
+//
+//    int n = 3;
+//    int tids[] = {tid1, tid2, tid3};
+//
+//    for (int i = 0; i < n; i++) {
+//        if (tids[i] == -1)
+//            exit(EXIT_FAILURE);
+//    }
+//
+//    for (int i = 0; i < n; i++) {
+//        if (thread_join(tids[i]) == -1)
+//            exit(EXIT_FAILURE);
+//    }
+//
+//    if (thread_libterminate() == -1)
+//        exit(EXIT_FAILURE);
+//
+//    exit(EXIT_SUCCESS);
+//}
