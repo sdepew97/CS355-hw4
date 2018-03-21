2fa5996f1353db3c7e17a38ae29c545adf34ec55
Sarah Depew <sdepew@brynmawr.edu>
2018-03-21 01:19:13 -0400

    Testing

--- test.c
+++ test.c
@@ -2,36 +2,17 @@
 #include <stdlib.h>
 #include "userthread.h"
 
-void foo(void *arg) {
-    printf("Hello world!\n");
-}
 
-/**
- * Simple FIFO test
- */
 int main(void) {
-    if (thread_libinit(FIFO) == -1)
-        exit(EXIT_FAILURE);
+    if (thread_libinit(FIFO) == -1) exit(EXIT_FAILURE);
 
 
-    int tid1 = thread_create(foo, NULL, 1);
-    int tid2 = thread_create(foo, NULL, 100);
-    int tid3 = thread_create(foo, NULL, -100);
+    printf("Test case for libinit and libterminate\n");
+    printf("Print \"You did it!\" on success.\n");
 
-    printf("Tesing invalid policy number\n");
-    printf("Should print exactly one 'Hello world' on success\n");
 
-    if (thread_join(tid1) < 0)
-        exit(EXIT_FAILURE);
+    if (thread_libterminate() == -1) exit(EXIT_FAILURE);
 
-    if (thread_join(tid2) < 0)
-        exit(EXIT_FAILURE);
-
-    if (thread_join(tid3) < 0)
-        exit(EXIT_FAILURE);
-
-    if (thread_libterminate() == -1)
-        exit(EXIT_FAILURE);
-
+    printf("You did it!\n");
     exit(EXIT_SUCCESS);
 }
