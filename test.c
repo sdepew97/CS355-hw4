6373fd17db7317c20c77aeab91a00d46b2e62970
Sarah Depew <sdepew@brynmawr.edu>
2018-03-21 02:45:05 -0400

    Testing

--- test.c
+++ test.c
@@ -1,51 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <poll.h>
 #include "userthread.h"
 
-void foo(void* args) {}
-void bar(void* args) {}
+#define FAIL -1
 
-int main(void) {
-    printf("Testing simple misuses\n");
-    printf("Shouldn't cause any crash on success\n");
-    thread_libterminate();
-    // the following three should return -1
-    if (thread_create(foo, NULL, 0) != -1)
+void big(void *arg)
+{
+    poll(NULL, 0, 200);
+}
+
+int main(void)
+{
+    if (thread_libinit(PRIORITY) == FAIL)
         exit(EXIT_FAILURE);
-    if (thread_create(bar, NULL, 0) != -1)
-        exit(EXIT_FAILURE);
-    if (thread_join(1) != -1)
-        exit(EXIT_FAILURE);
+    int tid1 = thread_create(big, NULL, -1);
+    int tid2 = thread_create(big, NULL, 0);
+    int tid3 = thread_create(big, NULL, 1);
+    int tid4 = thread_create(big, NULL, -1);
+    int tid5 = thread_create(big, NULL, 0);
+    int tid6 = thread_create(big, NULL, 1);
 
-    if (thread_libinit(FIFO) == -1)
-        exit(EXIT_FAILURE);
-
-    int tid1 = thread_create(foo, NULL, 0);
-    int tid2 = thread_create(foo, NULL, 0);
-    int tid3 = thread_create(foo, NULL, 0);
-    int tid4 = thread_create(foo, NULL, 0);
-    int tid5 = thread_create(foo, NULL, 0);
-    int tid6 = thread_create(foo, NULL, 0);
-
-    int n  = 6;
-    int tids[] = { tid1, tid2, tid3, tid4, tid5, tid6 };
-
-    for (int i = 0; i < n; i++)  {
-        if (tids[i] == -1)
+    printf("* A intense test case for PRIORITY scheduling policy handling a big job.\n");
+    printf("* Testing thread_create, thread_join and sigalarm working or not.\n");
+    printf("* You should see threads ending from highest to lowest and yielding for running too long.\n");
+    int num = 6;
+    int tids[] = {tid1, tid2, tid3, tid4, tid5, tid6};
+    for (int i = 0; i < num; i++)
+    {
+        if (tids[i] == FAIL)
             exit(EXIT_FAILURE);
     }
-
-    if (thread_join(tid6) == -1)
+    for (int i = 0; i < num; i++)
+    {
+        if (thread_join(tids[i]) == FAIL)
+            exit(EXIT_FAILURE);
+    }
+    if (thread_libterminate() == FAIL)
         exit(EXIT_FAILURE);
-
-    if (thread_libterminate() == -1)
-        exit(EXIT_FAILURE);
-
-
-    // more misuses...
-    thread_libterminate();
-    thread_join(123132);
-
 
     exit(EXIT_SUCCESS);
 }
