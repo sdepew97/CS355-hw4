49be82723ffaa5d61690b7e2060d2c296b466ecc
Sarah Depew <sdepew@brynmawr.edu>
2018-03-21 03:03:59 -0400

    Final Touches

--- test.c
+++ test.c
@@ -1,43 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <poll.h>
 #include "userthread.h"
 
-#define FAIL -1
-
-void big(void *arg)
-{
-    poll(NULL, 0, 300);
+void hello(void *arg) {
+    printf("%s\n", arg);
 }
 
-int main(void)
-{
-    if (thread_libinit(PRIORITY) == FAIL)
-        exit(EXIT_FAILURE);
-    int tid1 = thread_create(big, NULL, -1);
-    int tid2 = thread_create(big, NULL, 0);
-    int tid3 = thread_create(big, NULL, 1);
-    int tid4 = thread_create(big, NULL, -1);
-    int tid5 = thread_create(big, NULL, 0);
-    int tid6 = thread_create(big, NULL, 1);
+int main(void) {
+    if (thread_libinit(FIFO) == -1) exit(EXIT_FAILURE);
 
-    printf("* A intense test case for PRIORITY scheduling policy handling a big job.\n");
-    printf("* Testing thread_create, thread_join and sigalarm working or not.\n");
-    printf("* You should see threads ending from highest to lowest and yielding for running too long.\n");
-    int num = 6;
-    int tids[] = {tid1, tid2, tid3, tid4, tid5, tid6};
-    for (int i = 0; i < num; i++)
-    {
-        if (tids[i] == FAIL)
-            exit(EXIT_FAILURE);
-    }
-    for (int i = 0; i < num; i++)
-    {
-        if (thread_join(tids[i]) == FAIL)
-            exit(EXIT_FAILURE);
-    }
-    if (thread_libterminate() == FAIL)
+    char *hello_str = "Hello, world!";
+    int tid_1 = thread_create(NULL, hello_str, 0);
+
+    printf("Test case for FIFO given NULL as the funciton pointer.\n");
+    printf("Print \"Fail to create thread.\" on success.\n");
+
+    if(tid_1 == -1) {
+        printf("Fail to create thread.\n");
         exit(EXIT_FAILURE);
+    }
+
+    if (thread_join(tid_1) < 0) exit(EXIT_FAILURE);
+
+    if (thread_libterminate() == -1) exit(EXIT_FAILURE);
 
     exit(EXIT_SUCCESS);
 }
