181aba489abccfb43ccb7bf0f666e6fee6b8f452
Sarah Depew <sdepew@brynmawr.edu>
2018-03-21 02:32:13 -0400

    Testing

--- test.c
+++ test.c
@@ -2,50 +2,22 @@
 #include <stdlib.h>
 #include "userthread.h"
 
-void foo(void* args) {}
-void bar(void* args) {}
+void hello(void *arg) {
+    printf("%s\n", arg);
+}
 
 int main(void) {
-    printf("Testing simple misuses\n");
-    printf("Shouldn't cause any crash on success\n");
-    thread_libterminate();
-    // the following three should return -1
-    if (thread_create(foo, NULL, 0) != -1)
-        exit(EXIT_FAILURE);
-    if (thread_create(bar, NULL, 0) != -1)
-        exit(EXIT_FAILURE);
-    if (thread_join(1) != -1)
-        exit(EXIT_FAILURE);
+    if (thread_libinit(FIFO) == -1) exit(EXIT_FAILURE);
 
-    if (thread_libinit(FIFO) == -1)
-        exit(EXIT_FAILURE);
+    char *hello_str = "Hello, world!";
+    int tid_1 = thread_create(hello, hello_str, 0);
 
-    int tid1 = thread_create(foo, NULL, 0);
-    int tid2 = thread_create(foo, NULL, 0);
-    int tid3 = thread_create(foo, NULL, 0);
-    int tid4 = thread_create(foo, NULL, 0);
-    int tid5 = thread_create(foo, NULL, 0);
-    int tid6 = thread_create(foo, NULL, 0);
+    printf("Simplest test case for FIFO.\n");
+    printf("Print \"Hello, world!\" on success.\n");
 
-    int n  = 6;
-    int tids[] = { tid1, tid2, tid3, tid4, tid5, tid6 };
+    if (thread_join(tid_1) < 0) exit(EXIT_FAILURE);
 
-    for (int i = 0; i < n; i++)  {
-        if (tids[i] == -1)
-            exit(EXIT_FAILURE);
-    }
-
-    if (thread_join(tid6) == -1)
-        exit(EXIT_FAILURE);
-
-    if (thread_libterminate() == -1)
-        exit(EXIT_FAILURE);
-
-
-    // more misuses...
-    thread_libterminate();
-    thread_join(123132);
-
+    if (thread_libterminate() == -1) exit(EXIT_FAILURE);
 
     exit(EXIT_SUCCESS);
 }
