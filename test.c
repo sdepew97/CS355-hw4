b804f840e3c444907f16d681e49ec489d1192553
Sarah Depew <sdepew@brynmawr.edu>
2018-03-21 03:13:26 -0400

    Final Touches

--- test.c
+++ test.c
@@ -1,43 +1,54 @@
+//
+// Created by Sarah Depew on 3/19/18.
+//
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <poll.h>
 #include "userthread.h"
+#include "logger.h"
 
-#define FAIL -1
+#define SUCCESS 0
+#define FAILURE -1
+#define PRIORITY 1
 
-void big(void *arg)
-{
-    poll(NULL, 0, 200);
+void printHello () {
+    printf("Hello world\n");
 }
 
-int main(void)
-{
-    if (thread_libinit(PRIORITY) == FAIL)
-        exit(EXIT_FAILURE);
-    int tid1 = thread_create(big, NULL, -1);
-    int tid2 = thread_create(big, NULL, 0);
-    int tid3 = thread_create(big, NULL, 1);
-    int tid4 = thread_create(big, NULL, -1);
-    int tid5 = thread_create(big, NULL, 0);
-    int tid6 = thread_create(big, NULL, 1);
+void tryYield() {
+    printf("start yield\n");
+    thread_yield();
+    printf("end yield\n");
+    printHello();
+}
 
-    printf("* A intense test case for PRIORITY scheduling policy handling a big job.\n");
-    printf("* Testing thread_create, thread_join and sigalarm working or not.\n");
-    printf("* You should see threads ending from highest to lowest and yielding for running too long.\n");
-    int num = 6;
-    int tids[] = {tid1, tid2, tid3, tid4, tid5, tid6};
-    for (int i = 0; i < num; i++)
-    {
-        if (tids[i] == FAIL)
-            exit(EXIT_FAILURE);
-    }
-    for (int i = 0; i < num; i++)
-    {
-        if (thread_join(tids[i]) == FAIL)
-            exit(EXIT_FAILURE);
-    }
-    if (thread_libterminate() == FAIL)
+int main() {
+    if (thread_libinit(SJF) == FAILURE)
         exit(EXIT_FAILURE);
 
+    int tid1 = thread_create(printHello, NULL, -1);
+
+    if (tid1 == FAILURE)
+        exit(EXIT_FAILURE);
+
+    printf("joining 1\n");
+    if (thread_join(tid1) == FAILURE)
+        exit(EXIT_FAILURE);
+
+    int tid2 = thread_create(tryYield, NULL, -1);
+
+    if (tid2 == FAILURE)
+        exit(EXIT_FAILURE);
+
+    printf("joining 2\n");
+    if (thread_join(tid2) == FAILURE)
+        exit(EXIT_FAILURE);
+
+    printf("Back to main\n");
+
+    if (thread_libterminate() == FAILURE)
+        exit(EXIT_FAILURE);
+
+    printf("Congratulations, your test was successful!\n");
     exit(EXIT_SUCCESS);
 }
