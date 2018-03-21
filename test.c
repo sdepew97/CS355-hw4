253d5bb90480f85f0d1b397da240e0bfebab8453
Sarah Depew <sdepew@brynmawr.edu>
2018-03-21 00:27:38 -0400

    Testing

--- test.c
+++ test.c
@@ -14,14 +14,14 @@ int main(void) {
     printf("because it tests adding a thread with priority 100\n");
     printf("On success, it prints 'Woo!', does not crash, and does not cause any memory leaks\n");
 
-    int tidx = thread_create(FIFO, NULL, 0);
-    if (tidx != -1)
-        exit(EXIT_FAILURE);
+//    int tidx = thread_create(FIFO, NULL, 0);
+//    if (tidx != -1)
+//        exit(EXIT_FAILURE);
 
     // thread lib terminate could return success or failure,
     // just make sure no memory leaks or crashes occur
-//    thread_libterminate();
-//    thread_libterminate();
+    thread_libterminate();
+    thread_libterminate();
 
     if (thread_libinit(FIFO) == FAILURE) {
         exit(EXIT_FAILURE);
