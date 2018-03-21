8f032531d5d412d206d577e29148afdb77f15f81
Sarah Depew <sdepew@brynmawr.edu>
2018-03-21 09:15:21 -0400

    Morning Test

--- test.c
+++ test.c
@@ -2,7 +2,10 @@
 #include <stdlib.h>
 #include "userthread.h"
 
+int tid_1;
+
 void hello(void *arg) {
+    thread_join(tid_1);
     printf("%s\n", arg);
 }
 
@@ -10,15 +13,12 @@ int main(void) {
     if (thread_libinit(FIFO) == -1) exit(EXIT_FAILURE);
 
     char *hello_str = "Hello, world!";
-    int tid_1 = thread_create(hello, hello_str, 0);
+    tid_1 = thread_create(hello, hello_str, 0);
 
-    printf("Test case for FIFO. Attempt to join an non-existing thread.\n");
-    printf("Print \"Failed to join thread.\" on success.\n");
+    printf("Test case for FIFO. Let one thread join itself.\n");
+    printf("The program gets stuck and prints nothing on success.\n");
 
-    if (thread_join(-1) < 0) {
-        printf("Failed to join thread.\n");
-        exit(EXIT_FAILURE);
-    }
+    if (thread_join(tid_1) < 0) exit(EXIT_FAILURE);
 
     if (thread_libterminate() == -1) exit(EXIT_FAILURE);
 
