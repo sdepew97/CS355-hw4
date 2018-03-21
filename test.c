99d0f49d05956030d32804cfa8f1c8970fb67edf
Sarah Depew <sdepew@brynmawr.edu>
2018-03-21 00:24:03 -0400

    Testing

--- test.c
+++ test.c
@@ -2,29 +2,48 @@
 #include <stdlib.h>
 #include "userthread.h"
 
-void foo(void *arg) {
-    printf("%s\n", arg);
+#define FAILURE -1
+
+void foo() {
+    printf("Woo!\n");
 }
 
-/**
- * Simple FIFO test
- */
 int main(void) {
-    if (thread_libinit(FIFO) == -1)
+    printf("Testing some misuses of threads\n");
+    printf("This assumes that the priority queue range is from -1 to 1\n");
+    printf("because it tests adding a thread with priority 100\n");
+    printf("On success, it prints 'Woo!', does not crash, and does not cause any memory leaks\n");
+
+    int tidx = thread_create(FIFO, NULL, 0);
+    if (tidx != -1)
         exit(EXIT_FAILURE);
 
-    char *arg = "Yay!";
+    // thread lib terminate could return success or failure,
+    // just make sure no memory leaks or crashes occur
+    thread_libterminate();
+    thread_libterminate();
 
-    int tid1 = thread_create(foo, arg, 1);
-
-    printf("Basic FIFO test, testing only add and join.\n");
-    printf("Should print 'Yay!' if successful\n");
-
-    if (thread_join(tid1) < 0)
+    if (thread_libinit(FIFO) == FAILURE) {
         exit(EXIT_FAILURE);
+    }
 
-    if (thread_libterminate() == -1)
+    int tid1 = thread_create(fifo_misuse_foo, NULL, 100);
+    if (tid1 != FAILURE) {
         exit(EXIT_FAILURE);
+    }
+
+    tid1 = thread_create(fifo_misuse_foo, NULL, -1);
+    if (tid1 == FAILURE) {
+        exit(EXIT_FAILURE);
+    }
+
+    if (thread_join(tid1) == FAILURE) {
+        exit(EXIT_FAILURE);
+    }
+
+    if (thread_libterminate() == FAILURE) {
+        exit(EXIT_FAILURE);
+    }
 
     exit(EXIT_SUCCESS);
 }
